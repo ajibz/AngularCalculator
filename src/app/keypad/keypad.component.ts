c63f6aaff15a8b06641ecbc82bab636148da0b16
Bolarinwa Omoniyi <bolarinwaomoniyi@Bolarinwas-Air.fritz.box>
2023-10-17 11:44:15 +0200

    Calculator

--- src/app/keypad/keypad.component.ts
+++ src/app/keypad/keypad.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-keypad',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./keypad.component.css']
 })
 export class KeypadComponent {
+
+  @Output() outputCharacter = new EventEmitter<string>()
+
+  pressKey(keyCharacter:string) {
+  this.outputCharacter.emit(keyCharacter)
+  }
+
+  
 
 }
